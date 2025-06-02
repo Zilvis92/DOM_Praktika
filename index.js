14fdbd15b8727d98197d46b67fd1edef23483b0e
zilvinasblinovas <blinovaszilvinas@gmail.com>
2025-06-02 18:39:38 +0300

    JS push

---
+++ index.js
@@ -0,0 +1,3 @@
+// Pirmas div
+document.getElementById('pirma-antraste').innerHTML = 'Naujas pirmos anrastes tekstas';
+document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo paragrafo tekstas';
